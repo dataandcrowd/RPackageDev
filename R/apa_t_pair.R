d444958674d53e1f5c4aebb8394f604bbafda6d5
dataandcrowd <shinhyesop@gmail.com>
2022-11-01 07:04:12 +0000

    up

--- R/apa_t_pair.R
+++ R/apa_t_pair.R
@@ -1,3 +1,28 @@
+#' APA text for Paired-Samples T-Test
+#'
+#' @description
+#' Create APA-formatted text for the results of an independent-samples t-test in the following format:
+#'
+#' A paired-samples t-test was conducted to compare \{dv\} between \{level1\} (M = \{mean1\}, SD = \{sd1\}) and \{level2\} (M = \{mean2\}, SD = \{sd2\}). There was a \{non\}significant difference; t(\{df\}) = \{t_value\}, p = \{p_value\}.
+#' @param x A vector of the values for level 1.
+#' @param y A vector of the values for level 2.
+#' @param dv The text describing the DV in the output statement.
+#' @param level1 The text describing level 1 in the output statement.
+#' @param level2 The text describing level 2 in the output statement.
+#'
+#' @return A character string.
+#' @export
+#' @examples
+#' # use generic text
+#' apa_t_pair(x = self_res_att$f_self,
+#'            y = self_res_att$f_non)
+#'
+#' # specify the text for dv and levels
+#' apa_t_pair(x = self_res_att$f_self,
+#'            y = self_res_att$f_non,
+#'            dv = "preferences for female faces",
+#'            level1 = "participants who resembled those faces",
+#'            level2 = "non-self participants")
 apa_t_pair <- function(x, y,
                        dv = "the DV",
                        level1 = "level 1",
